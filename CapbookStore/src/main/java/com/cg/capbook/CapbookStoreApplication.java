50775fc9b893918c86c0bea78c208af716af4b00
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13073.corp.capgemini.com>
2019-03-19 12:52:21 +0530

    initial commit done by Shreyansh 19march2019

---
+++ CapbookStore/src/main/java/com/cg/capbook/CapbookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cg.capbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapbookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapbookStoreApplication.class, args);
+	}
+
+}
---
+++ CapbookStore/src/test/java/com/cg/capbook/CapbookStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cg.capbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapbookStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
