ec35b3a0eb7e3eff982fe6186afe80e1258fd7d2
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13074.corp.capgemini.com>
2019-03-20 11:56:51 +0530

    Aspect and EmailAlreadyRegisteredException by Aditi 20march2019

---
+++ CapbookStore/src/main/java/com/cg/capbook/exceptions/EmailAlreadyRegisteredException.java
@@ -0,0 +1,31 @@
+package com.cg.capbook.exceptions;
+
+public class EmailAlreadyRegisteredException extends Exception {
+
+	public EmailAlreadyRegisteredException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailAlreadyRegisteredException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailAlreadyRegisteredException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailAlreadyRegisteredException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailAlreadyRegisteredException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
