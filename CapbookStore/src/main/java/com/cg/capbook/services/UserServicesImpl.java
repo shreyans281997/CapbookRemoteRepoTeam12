caa3d494527845d528921338dcb106b88c451fc8
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13075.corp.capgemini.com>
2019-03-20 15:31:07 +0530

    encryption and decryption and Signup by ankush

--- CapbookStore/src/main/java/com/cg/capbook/services/UserServicesImpl.java
+++ CapbookStore/src/main/java/com/cg/capbook/services/UserServicesImpl.java
@@ -6,25 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cg.capbook.daoservice.UserDAO;
 import com.cg.capbook.exceptions.EmailAlreadyRegisteredException;
+import com.cg.capbook.exceptions.UserAccountNotFoundException;
 import com.cg.capbook.exceptions.invalidOTPException;
 import com.cg.capbook.model.UserAccount;
 
 public class UserServicesImpl implements IUserService{
 	@Autowired
 	private UserDAO userDao;
-	
+	@Autowired
+	private EncryptionAndDecryption encryptionAndDecryption;
 	@Override
 	public UserAccount acceptUserDetails(String emailId, String password, String firstName, String secondName, Date dateOfBirth, String gender, String mobileNo)
 			throws EmailAlreadyRegisteredException {
 		UserAccount userAccount=userDao.findById(emailId).orElse(null);
 		if(userAccount!=null)
 		throw new EmailAlreadyRegisteredException("Email is already in use.");
+		userAccount=new UserAccount(emailId, password, gender, firstName, secondName, mobileNo, dateOfBirth);
+		String encryptPassword=encryptionAndDecryption.encrypt(password);
+		userAccount.setPassword(encryptPassword);
 		return userDao.save(userAccount);
 	}
 
 	@Override
-	public UserAccount getUserDetails(String emailId) {
-		return null;
+	public UserAccount getUserDetails(String emailId) throws UserAccountNotFoundException {
+		return userDao.findById(emailId).orElseThrow(()->new UserAccountNotFoundException("Sorry this user is not part of our family"));
 		}
 
 	@Override
@@ -35,10 +40,7 @@ public class UserServicesImpl implements IUserService{
 
 	@Override
 	public String verifyOtp(int otp) throws invalidOTPException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
-
-	
-
 }
