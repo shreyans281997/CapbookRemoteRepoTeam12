51896dcb8a8a355cba7991314233bed7e8d65284
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13074.corp.capgemini.com>
2019-03-20 11:10:40 +0530

    Post And Comment class by Aditi

---
+++ CapbookStore/src/main/java/com/cg/capbook/model/Comment.java
@@ -0,0 +1,99 @@
+package com.cg.capbook.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKey;
+
+@Entity
+public class Comment {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private int commentId;
+	private String commentContent;
+	private String commentPerson;
+	@ManyToOne
+	@MapKey
+	@JoinColumn(name="postId")
+	private Post posts;
+	public Comment() {
+		super();
+	}
+	public Comment(int commentId, String commentContent, String commentPerson, Post posts) {
+		super();
+		this.commentId = commentId;
+		this.commentContent = commentContent;
+		this.commentPerson = commentPerson;
+		this.posts = posts;
+	}
+	public final int getCommentId() {
+		return commentId;
+	}
+	public final void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public final String getCommentContent() {
+		return commentContent;
+	}
+	public final void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public final String getCommentPerson() {
+		return commentPerson;
+	}
+	public final void setCommentPerson(String commentPerson) {
+		this.commentPerson = commentPerson;
+	}
+	public final Post getPosts() {
+		return posts;
+	}
+	public final void setPosts(Post posts) {
+		this.posts = posts;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((commentContent == null) ? 0 : commentContent.hashCode());
+		result = prime * result + commentId;
+		result = prime * result + ((commentPerson == null) ? 0 : commentPerson.hashCode());
+		result = prime * result + ((posts == null) ? 0 : posts.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (commentContent == null) {
+			if (other.commentContent != null)
+				return false;
+		} else if (!commentContent.equals(other.commentContent))
+			return false;
+		if (commentId != other.commentId)
+			return false;
+		if (commentPerson == null) {
+			if (other.commentPerson != null)
+				return false;
+		} else if (!commentPerson.equals(other.commentPerson))
+			return false;
+		if (posts == null) {
+			if (other.posts != null)
+				return false;
+		} else if (!posts.equals(other.posts))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", commentContent=" + commentContent + ", commentPerson="
+				+ commentPerson + "]";
+	}
+}
--- CapbookStore/src/main/java/com/cg/capbook/model/Post.java
+++ CapbookStore/src/main/java/com/cg/capbook/model/Post.java
@@ -1,5 +1,161 @@
 package com.cg.capbook.model;
 
-public class Post {
+import java.util.Arrays;
+import java.util.Map;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKey;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Post {
+	@Id
+	@SequenceGenerator(name="post_seq",sequenceName="post_seq",initialValue=101,allocationSize=10000)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="post_seq")
+	private int postId;
+	private String postContent;
+	@ManyToOne
+	private User user;
+	@Column(columnDefinition="BLOB")
+	private byte[] postPic;
+	private int noOfPostLikes;
+	private int noOfPostDislikes;
+	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	@MapKey
+	private Map<Integer, Comment> comments;
+	
+	public Post() {}
+
+	public Post(int postId, String postContent, User user, byte[] postPic, int noOfPostLikes, int noOfPostDislikes,
+			Map<Integer, Comment> comments) {
+		super();
+		this.postId = postId;
+		this.postContent = postContent;
+		this.user = user;
+		this.postPic = postPic;
+		this.noOfPostLikes = noOfPostLikes;
+		this.noOfPostDislikes = noOfPostDislikes;
+		this.comments = comments;
+	}
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getPostContent() {
+		return postContent;
+	}
+
+	public void setPostContent(String postContent) {
+		this.postContent = postContent;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public byte[] getPostPic() {
+		return postPic;
+	}
+
+	public void setPostPic(byte[] postPic) {
+		this.postPic = postPic;
+	}
+
+	public int getNoOfPostLikes() {
+		return noOfPostLikes;
+	}
+
+	public void setNoOfPostLikes(int noOfPostLikes) {
+		this.noOfPostLikes = noOfPostLikes;
+	}
+
+	public int getNoOfPostDislikes() {
+		return noOfPostDislikes;
+	}
+
+	public void setNoOfPostDislikes(int noOfPostDislikes) {
+		this.noOfPostDislikes = noOfPostDislikes;
+	}
+
+	public Map<Integer, Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(Map<Integer, Comment> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [postId=" + postId + ", postContent=" + postContent + ", user=" + user + ", postPic="
+				+ Arrays.toString(postPic) + ", noOfPostLikes=" + noOfPostLikes + ", noOfPostDislikes="
+				+ noOfPostDislikes + ", comments=" + comments + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result + noOfPostDislikes;
+		result = prime * result + noOfPostLikes;
+		result = prime * result + ((postContent == null) ? 0 : postContent.hashCode());
+		result = prime * result + postId;
+		result = prime * result + Arrays.hashCode(postPic);
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (noOfPostDislikes != other.noOfPostDislikes)
+			return false;
+		if (noOfPostLikes != other.noOfPostLikes)
+			return false;
+		if (postContent == null) {
+			if (other.postContent != null)
+				return false;
+		} else if (!postContent.equals(other.postContent))
+			return false;
+		if (postId != other.postId)
+			return false;
+		if (!Arrays.equals(postPic, other.postPic))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
 }
