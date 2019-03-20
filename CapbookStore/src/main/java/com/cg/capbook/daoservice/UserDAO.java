d9c1a38a65fd9aa6037cbcddd664c395c1b464cb
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13075.corp.capgemini.com>
2019-03-20 12:21:35 +0530

    Changes Done in commentDAO and userDAo by Ankush

--- CapbookStore/src/main/java/com/cg/capbook/daoservice/UserDAO.java
+++ CapbookStore/src/main/java/com/cg/capbook/daoservice/UserDAO.java
@@ -1,8 +1,8 @@
 package com.cg.capbook.daoservice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.cg.capbook.model.User;
+import com.cg.capbook.model.UserAccount;
 
-public interface UserDAO  extends JpaRepository<User, String>{
+public interface UserDAO  extends JpaRepository<UserAccount, String>{
 
 }
