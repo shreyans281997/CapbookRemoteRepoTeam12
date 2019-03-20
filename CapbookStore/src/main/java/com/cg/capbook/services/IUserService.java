b85eb0e473ff0ac9d9abbc5d9f35f5eaa336e054
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13075.corp.capgemini.com>
2019-03-20 15:06:46 +0530

    Add user and get user done by ankush

--- CapbookStore/src/main/java/com/cg/capbook/services/IUserService.java
+++ CapbookStore/src/main/java/com/cg/capbook/services/IUserService.java
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Component;
 
 import com.cg.capbook.exceptions.EmailAlreadyRegisteredException;
 import com.cg.capbook.exceptions.InvalidUsernameOrPasswordException;
+import com.cg.capbook.exceptions.UserAccountNotFoundException;
 import com.cg.capbook.exceptions.invalidOTPException;
 import com.cg.capbook.model.UserAccount;
 
 @Component("userService")
 public interface IUserService {
-	public UserAccount getUserDetails(String emailId);
+	public UserAccount getUserDetails(String emailId) throws UserAccountNotFoundException;
 	public UserAccount loginUser(String email, String password) throws InvalidUsernameOrPasswordException;
 	UserAccount acceptUserDetails(String emailId, String password, String firstName, String secondName,
 			Date dateOfBirth, String gender, String mobileNo) throws EmailAlreadyRegisteredException;
