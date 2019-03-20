4b51bd0e5e788e4163ca8c5de97589d25a80a0a4
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13075.corp.capgemini.com>
2019-03-20 14:12:11 +0530

    controllers by ankush

--- CapbookStore/src/main/java/com/cg/capbook/controllers/UserServiceController.java
+++ CapbookStore/src/main/java/com/cg/capbook/controllers/UserServiceController.java
@@ -1,5 +1,28 @@
 package com.cg.capbook.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cg.capbook.exceptions.EmailAlreadyRegisteredException;
+import com.cg.capbook.exceptions.InvalidUsernameOrPasswordException;
+
+@Controller
 public class UserServiceController {
 
+
+
+@RequestMapping("/showSignup")
+public ModelAndView signUp(@RequestParam String emailId,String password) throws EmailAlreadyRegisteredException {
+	return null;
+	     
+}
+
+@RequestMapping("/showLogin")
+public ModelAndView login(@RequestParam String emailId,String password) throws InvalidUsernameOrPasswordException{
+	return null;
+	
+}
 }
