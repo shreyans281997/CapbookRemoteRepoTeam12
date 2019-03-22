bbdf20b3c214ebfe422fbd47ddcb3d765e3b4412
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13074.corp.capgemini.com>
2019-03-22 09:58:35 +0530

    Aspect Updated Aditi 22march2019

--- CapbookStore/src/main/java/com/cg/capbook/aspect/CapBookExceptionAspect.java
+++ CapbookStore/src/main/java/com/cg/capbook/aspect/CapBookExceptionAspect.java
@@ -5,12 +5,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cg.capbook.exceptions.EmailAlreadyRegisteredException;
+import com.cg.capbook.exceptions.IncorrectOldPassword;
+import com.cg.capbook.exceptions.InvalidUsernameOrPasswordException;
+import com.cg.capbook.exceptions.UserAccountNotFoundException;
+import com.cg.capbook.exceptions.UserNameAlreadyExist;
 
 @ControllerAdvice(basePackages= {"com.cg.capbook.controllers"})
 public class CapBookExceptionAspect {
 	@ExceptionHandler(EmailAlreadyRegisteredException.class)
 	public ModelAndView handleEmailAlreadyRegisteredException(Exception e) {
-		return new ModelAndView("errorPage", "errorMessage", e.getMessage());
+		return new ModelAndView("loginPage", "errorMessage", e.getMessage());
+	}
+	@ExceptionHandler(IncorrectOldPassword.class)
+	public ModelAndView handleIncorrectOldPassword(Exception e) {
+		return new ModelAndView("changePassword", "errorMessage", e.getMessage());
+	}
+	@ExceptionHandler(InvalidUsernameOrPasswordException.class)
+	public ModelAndView handleInvalidUsernameOrPasswordException(Exception e) {
+		return new ModelAndView("loginPage", "errorMessage", e.getMessage());
+	}
+	@ExceptionHandler(UserAccountNotFoundException.class)
+	public ModelAndView handleUserAccountNotFoundException(Exception e) {
+		return new ModelAndView("loginPage", "errorMessage", e.getMessage());
+	}
+	@ExceptionHandler(UserNameAlreadyExist.class)
+	public ModelAndView handleUserNameAlreadyExist(Exception e) {
+		return new ModelAndView("getSignUp", "errorMessage", e.getMessage());
 	}
 	
 }
