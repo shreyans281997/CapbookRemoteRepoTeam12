64b70b35f2c39436149bc03ad0afa7645b06f2d5
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13073.corp.capgemini.com>
2019-03-22 10:03:07 +0530

    by shreyansh

--- CapbookStore/src/main/java/com/cg/capbook/aspect/CapBookExceptionAspect.java
+++ CapbookStore/src/main/java/com/cg/capbook/aspect/CapBookExceptionAspect.java
@@ -1,15 +1,12 @@
 package com.cg.capbook.aspect;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.cg.capbook.exceptions.EmailAlreadyRegisteredException;
 import com.cg.capbook.exceptions.IncorrectOldPassword;
 import com.cg.capbook.exceptions.InvalidUsernameOrPasswordException;
 import com.cg.capbook.exceptions.UserAccountNotFoundException;
 import com.cg.capbook.exceptions.UserNameAlreadyExist;
-
 @ControllerAdvice(basePackages= {"com.cg.capbook.controllers"})
 public class CapBookExceptionAspect {
 	@ExceptionHandler(EmailAlreadyRegisteredException.class)
