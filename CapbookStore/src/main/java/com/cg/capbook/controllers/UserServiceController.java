455961a95d567a33690fde95b45411ce34aed3a6
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13073.corp.capgemini.com>
2019-04-01 10:51:37 +0530

    by shreyansh

--- CapbookStore/src/main/java/com/cg/capbook/controllers/UserServiceController.java
+++ CapbookStore/src/main/java/com/cg/capbook/controllers/UserServiceController.java
@@ -123,12 +123,12 @@ public class UserServiceController {
 	@RequestMapping("/updatePost")
 	public ModelAndView updatePost(@RequestParam String postContent ,@SessionAttribute("user") UserAccount user) throws UserAccountNotFoundException, UserNotAFriendException {
 		postService.createPostText(user.getEmailId(), postContent);
-		List<Post> posts=postService.allPosts(user.getEmailId());
+		List<Post> posts=postService.showAllFriendsPosts(user.getEmailId());
 		return new ModelAndView("homePage", "posts", posts);
 	}
 	@RequestMapping("/showAllPost")
 	public ModelAndView allPosts(@SessionAttribute("user") UserAccount user) throws UserAccountNotFoundException, UserNotAFriendException {
-		List<Post> posts=postService.allPosts(user.getEmailId());
+		List<Post> posts=postService.showAllFriendsPosts(user.getEmailId());
 		return new ModelAndView("homePage", "posts", posts);
 	}
 	@RequestMapping("/getEditProfile")
@@ -145,7 +145,7 @@ public class UserServiceController {
 	public ModelAndView updateLikes(@SessionAttribute("user") UserAccount user,@SessionAttribute("posts") List<Post> posts, @RequestParam int postId, String likedBy) throws UserAccountNotFoundException, IncorrectOldPassword {
 		likeServices.updateLikes(postId, likedBy);
 		likeServices.getLikesCount(postId);
-		posts=postService.allPosts(user.getEmailId());
+		posts=postService.showAllFriendsPosts(user.getEmailId());
         return new ModelAndView("homePage","posts",posts);
 	}
 	@RequestMapping("/delEmail")
